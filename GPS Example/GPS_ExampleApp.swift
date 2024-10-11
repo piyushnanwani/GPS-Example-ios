b10f5d746f2f6de28ea7c272702dc14812b3c480
Piyush Nanwani <pnanwani61@gmail.com>
2024-10-11 16:49:16 +0530

    Initial Commit

---
+++ GPS Example/GPS_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPS_ExampleApp.swift
+//  GPS Example
+//
+//  Created by Piyush Nanwani on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GPS_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
